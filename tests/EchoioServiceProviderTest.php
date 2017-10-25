5bd5d798d4ccbc09366c06ae3bedb407c2f37faf
Isaac Esso <daprod2009@gmail.com>
2017-10-25 16:42:14 +0100

    WIP added new test

--- tests/EchoioServiceProviderTest.php
+++ tests/EchoioServiceProviderTest.php
@@ -2,11 +2,18 @@
 
 namespace Mediumart\Echoio\Tests;
 
+use Illuminate\Support\Arr;
+use Illuminate\Broadcasting\BroadcastController;
+
 class EchoioServiceProviderTest extends TestCase
 {
     /** @test */
     public function it_register_broadcasting_auth_route_middleware() 
     {
-        //
+        $router = $this->app['router'];
+        $this->assertTrue(Arr::has($router->getMiddleware(), 'echo.io'));
+        
+        $route =  $router->getRoutes()->getByAction(BroadcastController::class.'@authenticate');
+        $this->assertTrue(!! array_search('echo.io', $route->middleware()));
     }
 }
